4c3cfd9b6ae9fa7d4144dd58712d4414a2ce457b
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-04-14 00:35:07 +0000

    fixed code up

--- index.php
+++ index.php
@@ -4,8 +4,7 @@
 
     <!--Meta data-->
     <meta charset="utf-8">
-    <meta name="description" content="Area and Preimeter of a Semicircle Webpage">
-    <meta name="keywords" content="immaculata, ics2o">
+    <meta name="description" content="Area and Perimeter of a Semicircle Webpage">
     <meta name="author" content="Lily Carroll">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -13,20 +12,17 @@
     <link rel="apple-touch-icon.png" sizes="180x180" href="./fav/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="./fav/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./fav/favicon-16x16.png">
-    <link rel="manifest" href="./fav/site.webmanifest.png">
+    <link rel="manifest" href="./fav/site.webmanifest">
     <link rel="stylesheet" href="./css/style.css">
 
-    <!--Padding-->
-    <main style="padding-left: 20px; padding-right: 20px;">
-
     <!--Stylesheet-->
-      <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/style.css">
 
     <!--MDL theme-->
-      <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.red-pink.min.css" />
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.red-pink.min.css" />
 
     <!--Title-->
-      <title>Calculating the Area and Perimeter of a Semicircle</title>
+    <title>Calculating the Area and Perimeter of a Semicircle</title>
   </head>
   <body>
 
@@ -37,15 +33,15 @@
     <p>Welcome! This webpage calculates the area and perimeter of a semicircle. If you're unsure on how to calculate the area or perimeter of a semicircle, then you can just enter your values below and my webpage will calculate it for you! In the image below you can also see the formula for this calculation.</p>
 
     <!--Image-->
-    <center>
-      <img src="./images/formulaOfAreaAndPreimeterSemicircle.png" width="300" length="300" alt="Formula for the area and perimeter of semicircle">
-    </center>
+    <div style="text-align:center;">
+      <img src="./images/formulaOfAreaAndPreimeterSemicircle.png" width="300" height="300" alt="Formula for the area and perimeter of semicircle">
+    </div>
 
     <!--Question for user-->
-    <center>
-      <h6>Do you enjoy calculating the area and perimeter of different shapes?</h6>
+    <div style="text-align:center;">
+      <h5>Do you enjoy calculating the area and perimeter of different shapes?</h5>
   
-    <!--MDl radio button (on)-->
+      <!--MDl radio button (on)-->
       <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="option-1">
         <input type="radio" id="option-1" class="mdl-radio__button" name="options" value="1" checked>
         <span class="mdl-radio__label">Yes</span>
@@ -56,36 +52,35 @@
         <input type="radio" id="option-2" class="mdl-radio__button" name="options" value="2">
         <span class="mdl-radio__label">No</span>
       </label>
-    </center>
 
     <!--Form for user input-->
-    <center>
-      <h3>Please enter your input below:</h3>
-      <form action="./calculations.php" method="post" target="display-result">
-        <label for="radius">Raduis (cm):</label>
-        <input type="float" name="radius" placeholder="Input your radius here...">
-        <br>
-        <br>
+      <div class="center">
+        <h3>Please enter your input below:</h3>
+        <form action="./calculations.php" method="post" target="display-result">
+          <label for="radius">Radius (cm):</label>
+          <input type="number" name="radius" placeholder="Input your radius here...">
+      <br>
+      <br>
 
     <!--Accent-colored raised button with ripple-->
-        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-          Calculate
-        </button>
-      </form>
-    </center>
+          <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+            Calculate
+          </button>
+        </form>
+      </div>
 
     <!--Iframe to display results-->
-    <iframe id="display-result" name="display-result"></iframe>
+      <iframe id="display-result" name="display-result"></iframe>
 
     <!--Slider with Starting Value-->
-    <br>
-    <br>
-    <center>
-      <h5>Please leave me feedback by adjusting the slider on how helpful you found this calculation webpage.</h5>
-    </center>
-    <br>
+      <br>
+      <br>
+      <div class="center">
+        <h5>Please leave me feedback by adjusting the slider on how helpful you found this calculation webpage.</h5>
+      </div>
+      <br>
 
     <!--MDL Slider with Starting Value-->
-    <input class="mdl-slider mdl-js-slider" type="range" min="0" max="100" value="25" tabindex="0">
+      <input class="mdl-slider mdl-js-slider" type="range" min="0" max="100" value="25" tabindex="0">
   </body>
 </html>
